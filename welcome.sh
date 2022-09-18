8cc0f2af2234b1365d0b6e874c6d2f3e580dad66
Bino Faata <maximleluk@gmail.com>
2022-09-18 19:34:00 +0300

    spacefixes

--- welcome.sh
+++ welcome.sh
@@ -21,9 +21,9 @@ echo -e "\e[33mSysInfo ---------------------------------             Server Serv
 echo -e "\e[32mUPTIME: \e[0m" $(uptime -p) "                       \e[32mHTTPD: \e[0m" $(systemctl show -p ActiveState --value "httpd")
 echo -e "\e[32mHOSTNAME: \e[0m" $(hostname) "                                 \e[32mMySQL: \e[0m" $(systemctl show -p ActiveState --value "mysql")
 echo -e "\e[32mDISTRIBUTIVE: \e[0m" $(cat /etc/os-release | grep "PRETTY_NAME" | cut -d "=" -f 2- | sed 's/"//g') "               \e[32mMySQLd: \e[0m" $(systemctl show -p ActiveState --value "mysqld")
-echo -e "\e[32mCPU LOAD: \e[0m" $(cat /proc/loadavg | awk '{print $1 ", " $2 ", " $3}') "                         \e[32mFirewalld: \e[0m" $(systemctl show -p ActiveState --value "firewalld")
-echo -e "\e[32mFREE MEMORY: \e[0m" $(free -m | head -n 2 | tail -n 1 | awk {'print $4'})"Mb" "                                 \e[32mCrond: \e[0m" $(systemctl show -p ActiveState --value "crond")
-echo -e "\e[32mFREE DISK SPACE: \e[0m" $(df -h / | awk '{ a = $2 } END { print a }') "                               \e[32miptables: \e[0m" $(systemctl show -p ActiveState --value "iptables")
+echo -e "\e[32mCPU LOAD: \e[0m" $(cat /proc/loadavg | awk '{print $1 ", " $2 ", " $3}') "                          \e[32mFirewalld: \e[0m" $(systemctl show -p ActiveState --value "firewalld")
+echo -e "\e[32mFREE MEMORY: \e[0m" $(free -m | head -n 2 | tail -n 1 | awk {'print $4'})"Mb" "                                  \e[32mCrond: \e[0m" $(systemctl show -p ActiveState --value "crond")
+echo -e "\e[32mFREE DISK SPACE: \e[0m" $(df -h / | awk '{ a = $2 } END { print a }') "                                \e[32miptables: \e[0m" $(systemctl show -p ActiveState --value "iptables")
 echo ''
 echo -e "\e[33mUseful params ---------------------------\e[0m"
 echo -e "\e[32mForward status: \e[0m" $(sysctl net.ipv4.ip_forward)
